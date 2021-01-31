135c56c51c4cf510c14064c0755714494fef1d96
Karabo Pheko <216021429@student.uj.ac.za>
2021-01-31 03:00:46 -0800

    End of section 2

---
+++ API/Controllers/-- SQLite.sql
@@ -0,0 +1,9 @@
+-- SQLite
+INSERT or REPLACE INTO Users (Id, UserName)
+VALUES (1, "Karabo");
+
+INSERT or REPLACE INTO Users (Id, UserName)
+VALUES (2, "Bob");
+
+INSERT or REPLACE INTO Users (Id, UserName)
+VALUES (3, "Jain");
